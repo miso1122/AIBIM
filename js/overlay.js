9a5c8e85683b4da5edfc1c3d9163e30aeea78897
miso1122 <89561263+miso1122@users.noreply.github.com>
2022-09-06 18:47:31 +0900

    Update overlay.js

--- js/overlay.js
+++ js/overlay.js
@@ -1,7 +1,7 @@
       // 버튼 맨 앞으로
       AFRAME.registerComponent("overlay", {
         dependencies: ['material'],
-        tick: function () {
+        tick: function (time, timeDelta) {
           this.el.sceneEl.renderer.sortObjects = true;
           this.el.object3D.renderOrder = 1000;
           this.el.components.material.material.depthTest = false;
@@ -11,7 +11,7 @@
       // 글씨 맨 앞으로
       AFRAME.registerComponent("overlay_text", {
         dependencies: ['material'],
-        tick: function () {
+        tick: function (time, timeDelta) {
           this.el.sceneEl.renderer.sortObjects = true;
           this.el.object3D.renderOrder = 1000;
           this.el.components.text.material.depthTest = false;
