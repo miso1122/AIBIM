39d0a54668490ad705aab706179f2cfc5f262fae
miso1122 <89561263+miso1122@users.noreply.github.com>
2022-09-06 17:44:46 +0900

    Update overlay.js

--- js/overlay.js
+++ js/overlay.js
@@ -5,6 +5,7 @@
           this.el.sceneEl.renderer.sortObjects = true;
           this.el.object3D.renderOrder = 1000;
           this.el.components.material.material.depthTest = false;
+          this.el.components.material.material.depthWrite = false;
         }
       });
       // 글씨 맨 앞으로
@@ -14,5 +15,6 @@
           this.el.sceneEl.renderer.sortObjects = true;
           this.el.object3D.renderOrder = 1000;
           this.el.components.text.material.depthTest = false;
+          this.el.components.text.material.depthWrite = false;
         }
       });
