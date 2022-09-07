5e54f3b324c75ec399f0e82bdbaff3486348d207
miso1122 <89561263+miso1122@users.noreply.github.com>
2022-09-07 10:05:22 +0900

    Update overlay.js

--- js/overlay.js
+++ js/overlay.js
@@ -1,13 +1,15 @@
-      // 버튼 맨 앞으로
+    // 버튼 맨 앞으로
       AFRAME.registerComponent("overlay", {
         dependencies: ['material'],
         tick: function (time, timeDelta) {
           this.el.sceneEl.renderer.sortObjects = true;
           this.el.object3D.renderOrder = 1000;
-         
-          this.el.components.material.material.depthTest = false;
-          this.el.components.material.material.depthWrite = false;
-
+          this.el.components.material.material.polygonOffset = true;
+          this.el.components.material.material.polygonOffsetUnit = 1;
+          this.el.components.material.material.polygonOffsetFactor = 1;
+//           this.el.components.material.material.depthTest = false;
+//           this.el.components.material.material.depthWrite = false;
+//           this.el.components.material.material.blending = THREE.AdditiveBlending
         }
       });
       // 글씨 맨 앞으로
@@ -16,9 +18,11 @@
         tick: function (time, timeDelta) {
           this.el.sceneEl.renderer.sortObjects = true;
           this.el.object3D.renderOrder = 1000;
-      
-          this.el.components.text.material.depthTest = false;
-          this.el.components.text.material.depthWrite = false;
-
+          this.el.components.text.material.polygonOffset = true;
+          this.el.components.text.material.polygonOffsetUnit = 1;
+          this.el.components.text.material.polygonOffsetFactor = 1;
+//           this.el.components.text.material.depthTest = false;
+//           this.el.components.text.material.depthWrite = false;
+//           this.el.components.text.material.blending = THREE.AdditiveBlending
         }
       });
